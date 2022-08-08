1ab858bc6a6a2a6ceb85204d67e3da66d6d5b9b7
KS Bhat <kumarasubrahmanya.h@gmail.com>
2022-08-09 00:54:04 +0530

    Committing working tic tac toe game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import Board from './Board';
+import Info from "./Info";
+
+import "./css/app.css";
 import './App.css';
 
+import { useState } from 'react';
+
 function App() {
+   // Creating a reset state, which indicates whether the game should be reset or not
+   const [reset, setReset] = useState(false);
+  
+   // Creating a winner state, which indicates the current winner
+  const [winner, setWinner] = useState('');
+  const resetBoard = () => {
+    setReset(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          {/* Shrinks the popup when there is no winner */}
+          <div className={`winner ${winner !== '' ? '' : 'shrink'}`}>
+              {/* Display the current winner */}
+              <div className='winner-text'>{winner}</div>
+              {/* Button used to reset the board */}
+              <button onClick={() => resetBoard()}>
+                  Reset Board
+              </button>
+          </div>
+          {/* Custom made board component comprising of the tic-tac-toe board  */}
+          <Board reset={reset} setReset={setReset} winner={winner} 
+              setWinner={setWinner} />
+          <Info />
+      </div>
   );
 }
 
